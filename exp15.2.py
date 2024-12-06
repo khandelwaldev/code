eeca978b5239845dbc8ba7fa14ad8343030f643c
Dev Khandelwal <122960451+khandelwaldev@users.noreply.github.com>
2024-12-06 11:55:02 +0530

    Update

---
+++ exp15.2.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt 
+
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+
+plt.scatter(x, y)
+
+plt.title("simple scatter plot")
+plt.xlabel("x values")
+plt.ylabel("y values")
+
+plt.show()
